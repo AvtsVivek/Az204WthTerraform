016f37b0c64a3e632103c35283f9b02a400cbbf3
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2022-08-10 08:59:21 +0530

    Added some comments

--- src/dotnet-apps/simple-webapp/Program.cs
+++ src/dotnet-apps/simple-webapp/Program.cs
@@ -23,3 +23,14 @@ app.UseAuthorization();
 app.MapRazorPages();
 
 app.Run();
+
+// app.Run("http://127.0.0.1:5000");
+
+//if (app.Environment.IsDevelopment())
+//{
+//    app.Run();
+//}
+//else
+//{
+//    app.Run("http://127.0.0.1:5000");
+//}
