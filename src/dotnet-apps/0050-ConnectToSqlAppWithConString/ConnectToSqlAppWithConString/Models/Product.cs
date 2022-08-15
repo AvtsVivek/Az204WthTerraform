62b9d1be684598a175e728f7152894a7775f4ca2
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2022-08-15 19:38:25 +0530

    Here we go

--- src/dotnet-apps/0050-ConnectToSqlAppWithConString/ConnectToSqlAppWithConString/Models/Product.cs
+++ src/dotnet-apps/0050-ConnectToSqlAppWithConString/ConnectToSqlAppWithConString/Models/Product.cs
@@ -3,7 +3,7 @@
     public class Product
     {
         public int ProductID { get; set; }
-        public string ProductName { get; set; }    
+        public string ProductName { get; set; } = default!;
         public int Quantity { get; set; }
     }
 }
