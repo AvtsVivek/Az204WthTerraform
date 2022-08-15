43018aac96ea6fbe0ac0ed20d2d1c3c554ef6b19
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2022-08-15 13:25:53 +0530

    edited the yaml file to point to correct dotnet app

--- src/tf-files/610100-simple-web-app-to-linux-vm-apche/commands.sh
+++ src/tf-files/610100-simple-web-app-to-linux-vm-apche/commands.sh
@@ -5,7 +5,9 @@ cd ./src/tf-files/610100-simple-web-app-to-linux-vm-apche/
 
 cd ../../..
 
-dotnet run --project ./../../dotnet-apps/simple-webapp/simple-webapp.csproj
+dotnet run --project ./../../dotnet-apps/0020-simple-webapp/simple-webapp.csproj
+
+dotnet build ./../../dotnet-apps/0020-simple-webapp/simple-webapp.csproj
 
 # First ensure the simple web app is published. 
 # Run either of the following two comands
