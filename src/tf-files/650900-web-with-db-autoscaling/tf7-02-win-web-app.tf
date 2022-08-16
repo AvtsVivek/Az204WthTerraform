a1e163738f987850a5cd3f204f2f36c62f54a48a
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2022-08-16 09:48:23 +0530

    Added auto scaling

--- src/tf-files/650900-web-with-db-autoscaling/tf7-02-win-web-app.tf
+++ src/tf-files/650900-web-with-db-autoscaling/tf7-02-win-web-app.tf
@@ -41,3 +41,54 @@ resource "azurerm_windows_web_app" "windows_web_app" {
     failed_request_tracing = true
   }
 }
+
+resource "azurerm_monitor_autoscale_setting" "service_plan_auto_scale" {
+  name                = "myAutoscaleSetting"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  target_resource_id  = azurerm_service_plan.service_plan.id
+  profile {
+    name = "default"
+    capacity {
+      default = 1
+      minimum = 1
+      maximum = 10
+    }
+    rule {
+      metric_trigger {
+        metric_name        = "CpuPercentage"
+        metric_resource_id = azurerm_service_plan.service_plan.id
+        time_grain         = "PT1M"
+        statistic          = "Average"
+        time_window        = "PT5M"
+        time_aggregation   = "Average"
+        operator           = "GreaterThan"
+        threshold          = 90
+      }
+      scale_action {
+        direction = "Increase"
+        type      = "ChangeCount"
+        value     = "1"
+        cooldown  = "PT1M"
+      }
+    }
+    rule {
+      metric_trigger {
+        metric_name        = "CpuPercentage"
+        metric_resource_id = azurerm_service_plan.service_plan.id
+        time_grain         = "PT1M"
+        statistic          = "Average"
+        time_window        = "PT5M"
+        time_aggregation   = "Average"
+        operator           = "LessThan"
+        threshold          = 10
+      }
+      scale_action {
+        direction = "Decrease"
+        type      = "ChangeCount"
+        value     = "1"
+        cooldown  = "PT1M"
+      }
+    }
+  }  
+}
