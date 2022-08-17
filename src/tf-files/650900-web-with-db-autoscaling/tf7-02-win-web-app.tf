92e30dd96fd6cc02f1a3a86813abd7de795a6a9d
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2022-08-17 10:03:06 +0530

    Added new screen shots

--- src/tf-files/650900-web-with-db-autoscaling/tf7-02-win-web-app.tf
+++ src/tf-files/650900-web-with-db-autoscaling/tf7-02-win-web-app.tf
@@ -43,6 +43,11 @@ resource "azurerm_windows_web_app" "windows_web_app" {
 }
 
 resource "azurerm_monitor_autoscale_setting" "service_plan_auto_scale" {
+
+  # depends_on = [
+  #   azurerm_windows_web_app.windows_web_app
+  # ]
+
   name                = "myAutoscaleSetting"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
@@ -90,5 +95,5 @@ resource "azurerm_monitor_autoscale_setting" "service_plan_auto_scale" {
         cooldown  = "PT1M"
       }
     }
-  }  
+  }
 }
