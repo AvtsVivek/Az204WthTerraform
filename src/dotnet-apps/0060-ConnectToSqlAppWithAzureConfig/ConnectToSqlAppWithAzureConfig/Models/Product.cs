72e353e9ce946ea7bbb69b57b91210ab14d52ffb
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2022-08-19 09:34:23 +0530

    Added a new exercise

---
+++ src/dotnet-apps/0060-ConnectToSqlAppWithAzureConfig/ConnectToSqlAppWithAzureConfig/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace sqlapp.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; } = default!;
+        public int Quantity { get; set; }
+    }
+}
