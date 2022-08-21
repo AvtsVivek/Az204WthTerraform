57eb9a7550342a6624e7aa399335db0a4e7e19fd
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2022-08-21 18:51:54 +0530

    Added images

--- src/tf-files/700110-azure-func-tut1/ReadMe.md
+++ src/tf-files/700110-azure-func-tut1/ReadMe.md
@@ -1,4 +1,9 @@
 
 - This creates azure function with terraform 
   
-- 
+- The we will create a js based Azure Function in the /src/js-apps folder
+  
+- https://docs.microsoft.com/en-us/azure/azure-functions/functions-develop-vs-code?tabs=nodejs
+
+- The following Pre requisties are needed.
+https://docs.microsoft.com/en-us/azure/azure-functions/functions-develop-vs-code?tabs=nodejs#prerequisites
---
+++ src/tf-files/700110-azure-func-tut1/azure-app.sh
@@ -0,0 +1,22 @@
+cd ../../..
+
+# cd into the directory.
+cd ./src/tf-files/700110-azure-func-tut1/
+
+# Ensure necessary visual studio code extensions are installed. See InstallExtensionToVisualStudioCode.jpg in the images folder.
+
+# The follwing is the tutorial that is followed.
+
+https://docs.microsoft.com/en-us/azure/azure-functions/functions-develop-vs-code?tabs=nodejs
+
+# The following Pre requisties are needed.
+https://docs.microsoft.com/en-us/azure/azure-functions/functions-develop-vs-code?tabs=nodejs#prerequisites
+
+cd ../../../src/js-apps
+
+mkdir js-azure-func-app
+
+cd js-azure-func-app
+
+code . -r
+
