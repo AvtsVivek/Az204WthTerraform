a0f1b5f1a7168f6fe576e3ecad4d6e637763d0ba
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2022-08-23 09:36:09 +0530

    Added new project

---
+++ src/dotnet-apps/0100-AzureFuncGetProductsFromSqlDb/AzureFuncGetProductsFromSqlDb/GetProduct.cs
@@ -0,0 +1,56 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+
+namespace sqlfunction
+{
+    public static class GetProduct
+    {
+        [FunctionName("GetProduct")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("Get data from the database");
+            List<Product> _product_lst = new List<Product>();
+            string _statement = "SELECT ProductID,ProductName,Quantity from Products";
+            SqlConnection _connection = GetConnection();
+
+            _connection.Open();
+
+            SqlCommand _sqlcommand = new SqlCommand(_statement, _connection);
+
+            using (SqlDataReader _reader = _sqlcommand.ExecuteReader())
+            {
+                while (_reader.Read())
+                {
+                    Product _product = new Product()
+                    {
+                        ProductID = _reader.GetInt32(0),
+                        ProductName = _reader.GetString(1),
+                        Quantity = _reader.GetInt32(2)
+                    };
+
+                    _product_lst.Add(_product);
+                }
+            }
+            _connection.Close();            
+
+            return new OkObjectResult(_product_lst);
+        }
+
+        private static SqlConnection GetConnection()
+        {
+            var connectionString = "Server=tcp:hr-dev-ms-sql-server.database.windows.net,1433;Initial Catalog=hr-dev-ms-sql-db;Persist Security Info=False;User ID=adm1n157r470r;Password=4-v3ry-53cr37-p455w0rd;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            return new SqlConnection(connectionString);
+        }
+    }
+}
---
+++ src/tf-files/800120-azure-sql-func/ReadMe.md
@@ -0,0 +1,8 @@
+- This demonistrates deployment slots. 
+
+- This builds on (earlier example)[https://github.com/AvtsVivek/Az204WthTerraform/tree/main/src/tf-files/650940-web-with-db-deployment-slots] but with only sql resources. 
+
+- This provisions sql server with database. Then you have to allow access to the clinet. See the images.
+
+- Then run the dotnet azure function 0100-AzureFuncGetProductsFromSqlDb application. the 
+
