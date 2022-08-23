9cb6d68d865af263b8fd0b2e4b7b8ccf524137f5
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2022-08-23 14:53:55 +0530

    Aded new project

---
+++ src/tf-files/800150-azure-func-dotnet-conn-string-env-var-asdf/ReadMe.md
@@ -0,0 +1,10 @@
+
+- This builds from the [earlier exmaple](https://github.com/AvtsVivek/Az204WthTerraform/tree/main/src/tf-files/700120-azure-func-dotnet), but adds a connection string.
+
+- Once provisioned, add data to database.
+
+- Deploy [this app - 0130-AzureFuncConnStringEnvVar](https://github.com/AvtsVivek/Az204WthTerraform/tree/main/src/dotnet-apps/0130-AzureFuncConnStringEnvVar)
+
+- Note that this app, [doest contain a hard coded connection string. But instead picks from evn var](https://github.com/AvtsVivek/Az204WthTerraform/blob/f49e733549bb602b550b277247e01eeaf19624f6/src/dotnet-apps/0130-AzureFuncConnStringEnvVar/AzureFuncConnStringEnvVar/GetProduct.cs#L53).
+
+- 
---
+++ src/tf-files/800150-azure-func-dotnet-conn-string-env-var-asdf/tf10-azure-function.tf
@@ -0,0 +1,36 @@
+
+variable "win_function_app_name" {
+  description = "Windows Function App name"
+  type        = string
+  default     = "win-func-app"
+}
+
+resource "azurerm_windows_function_app" "win_function_app" {
+  name                = "${local.resource_name_prefix}-${var.win_function_app_name}-${random_string.myrandom.id}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+
+  storage_account_name       = azurerm_storage_account.storage_account_for_azure_func.name
+  storage_account_access_key = azurerm_storage_account.storage_account_for_azure_func.primary_access_key
+  service_plan_id            = azurerm_service_plan.service_plan_for_azure_func.id
+
+  # app_service_plan_id        = azurerm_app_service_plan.app_service_plan.id
+  app_settings = {
+    "WEBSITE_RUN_FROM_PACKAGE"       = "",
+    "FUNCTIONS_WORKER_RUNTIME"       = "dotnet"
+    "APPINSIGHTS_INSTRUMENTATIONKEY" = azurerm_application_insights.application_insights.instrumentation_key,
+    FUNCTIONS_EXTENSION_VERSION      = "~4"
+  }
+
+  connection_string {
+    name  = "SQLConnection"
+    type  = "SQLAzure"
+    value = "Server=tcp:hr-dev-ms-sql-server.database.windows.net,1433;Initial Catalog=hr-dev-ms-sql-db;Persist Security Info=False;User ID=adm1n157r470r;Password=4-v3ry-53cr37-p455w0rd;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+  }
+
+  site_config {
+    application_stack {
+      dotnet_version = 6.0
+    }
+  }
+}
