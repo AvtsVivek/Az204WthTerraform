3b247e74aa2d7f30609dd1c83cfc68dfc2c59587
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2022-09-10 14:40:22 +0530

    Added new files

--- src/tf-files/810150-azure-container-registry/ReadMe.md
+++ src/tf-files/810150-azure-container-registry/ReadMe.md
@@ -1,3 +1,18 @@
 Azure Container Registry
 
 - Objective is to deploy a azure conainer registry, then create an image for a dotnet application. Then push that to the container registry. Then pull that image and run it.
+
+- Overall, we push three images.
+
+- Azure Container Registry 
+![Azure Container Registry](./images/AzureAcrLoginServer1.jpg)
+
+- Azure Container Registry 
+![Azure Container Registry](./images/AzureAcrLoginServer2.jpg)
+
+- Azure Container Registry 
+![Azure Container Registry](./images/AzureAcrLoginServer3.jpg)
+
+- Azure Container Registry 
+![Azure Container Registry](./images/AzureAcrLoginServer4.jpg)
+
--- src/tf-files/810150-azure-container-registry/commands.sh
+++ src/tf-files/810150-azure-container-registry/commands.sh
@@ -19,6 +19,42 @@ terraform state list
 
 # Once successifully provisioned, the next step is to build an image so we can push it.
 
+# 1. First get a hello world image
+
+docker container ls -a
+
+docker pull mcr.microsoft.com/hello-world
+
+# Go to the azure portal, then acr and then get the login server.
+
+az acr login --name acrvglbmi
+
+# Ensure you get login succeeded.
+
+# docker tag mcr.microsoft.com/hello-world <login-server>/hello-world:v1
+
+docker tag mcr.microsoft.com/hello-world acrvglbmi.azurecr.io/hello-world:v1
+
+# docker push <login-server>/hello-world:v1
+
+docker push acrvglbmi.azurecr.io/hello-world:v1
+
+# Now, check in the portal, the remove the local image
+
+docker rmi mcr.microsoft.com/hello-world acrvglbmi.azurecr.io/hello-world:v1
+
+# OR
+
+docker image rm mcr.microsoft.com/hello-world acrvglbmi.azurecr.io/hello-world:v1
+
+docker image ls
+
+docker pull acrvglbmi.azurecr.io/hello-world:v1
+
+docker image ls
+
+# 2. First build a local image
+
 docker run --name mynginxdefault -p 81:80 -d nginx
 
 docker container ls -a
@@ -41,6 +77,40 @@ docker build -t avts/nginxvivek:v1 .
 
 docker run --name viveknginx -p 81:80 -d avts/nginxvivek:v1
 
+# Now browse to localhost:81 to test and see
+
+docker container stop viveknginx
+
+docker container ls
+
+docker container ls -a
+
+docker container rm viveknginx
+
+docker tag avts/nginxvivek:v1 acrvglbmi.azurecr.io/nginxvivek:v1
+
+# docker push <login-server>/hello-world:v1
+
+docker push acrvglbmi.azurecr.io/nginxvivek:v1
+
+# Now remove the local image
+
+docker rmi avts/nginxvivek:v1 acrvglbmi.azurecr.io/nginxvivek:v1
+
+# OR
+
+docker image rm avts/nginxvivek:v1 acrvglbmi.azurecr.io/nginxvivek:v1
+
+docker image ls
+
+docker pull acrvglbmi.azurecr.io/nginxvivek:v1
+
+docker image ls
+
+docker run acrvglbmi.azurecr.io/nginxvivek:v1
+
+docker run --name viveknginx -p 81:80 -d acrvglbmi.azurecr.io/nginxvivek:v1
+
 # Now browse to localhost:81
 
 docker container stop viveknginx
@@ -53,35 +123,68 @@ docker container rm viveknginx
 
 docker container ls -a
 
-docker pull mcr.microsoft.com/hello-world
+# Verify in the acr on portal, there should be two images
 
-# Go to the azure portal, then acr and then get the login server.
+# 3. 
 
-az acr login --name acrpgotfe
+\dotnet-apps\0020-simple-webapp\Dockerfile
 
-# Ensure you get login succeeded.
+# cd into the directory.
+cd ./../../dotnet-apps/0020-simple-webapp
 
-# docker tag mcr.microsoft.com/hello-world <login-server>/hello-world:v1
+docker build -t simplewebapp:v1 .
 
-docker tag mcr.microsoft.com/hello-world acrpgotfe.azurecr.io/hello-world:v1
+docker run --name viveksimplewebapp -p 81:80 -d simplewebapp:v1
+
+# Now browse to localhost:81 to test and see. If you feel there is caching, then try refreshing again and again.
+
+docker container stop viveksimplewebapp
+
+docker container ls
+
+docker container ls -a
+
+docker container rm viveksimplewebapp
+
+docker tag simplewebapp:v1 acrvglbmi.azurecr.io/simplewebapp:v1
 
 # docker push <login-server>/hello-world:v1
 
-docker push acrpgotfe.azurecr.io/hello-world:v1
+docker push acrvglbmi.azurecr.io/simplewebapp:v1
 
 # Now remove the local image
 
-docker rmi mcr.microsoft.com/hello-world acrpgotfe.azurecr.io/hello-world:v1
+docker rmi simplewebapp:v1 acrvglbmi.azurecr.io/simplewebapp:v1
 
 # OR
 
-docker image rm mcr.microsoft.com/hello-world acrpgotfe.azurecr.io/hello-world:v1
+docker image rm simplewebapp:v1 acrvglbmi.azurecr.io/simplewebapp:v1
 
 docker image ls
 
-docker pull acrpgotfe.azurecr.io/hello-world:v1
+docker pull acrvglbmi.azurecr.io/simplewebapp:v1
 
 docker image ls
+
+docker run --name viveksimpledotnetapp -p 81:80 -d acrvglbmi.azurecr.io/simplewebapp:v1
+
+# Now browse to localhost:81
+
+docker container stop viveksimpledotnetapp
+
+docker container ls
+
+docker container ls -a
+
+docker container rm viveksimpledotnetapp
+
+docker container ls -a
+
+# Verify in the acr on portal, there should be three images
+
+#######################################################################################
+
+cd ./../../tf-files/810150-azure-container-registry
 
 terraform state show 
 
