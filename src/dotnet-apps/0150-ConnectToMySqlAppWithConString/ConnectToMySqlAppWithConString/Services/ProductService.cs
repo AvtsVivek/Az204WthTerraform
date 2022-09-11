ad37a8f268b65ebabca83a715cc4cd313784c95a
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2022-09-11 21:38:40 +0530

    Added a New Project

---
+++ src/dotnet-apps/0150-ConnectToMySqlAppWithConString/ConnectToMySqlAppWithConString/Services/ProductService.cs
@@ -0,0 +1,49 @@
+﻿using sqlapp.Models;
+using MySql.Data.MySqlClient;
+
+namespace sqlapp.Services
+{
+
+    // This service will interact with our Product data in the SQL database
+    public class ProductService : IProductService
+    {
+        private readonly IConfiguration _configuration;
+        public ProductService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        private MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(_configuration.GetConnectionString("MySqlConnection"));
+        }
+        public List<Product> GetProducts()
+        {
+            List<Product> _product_lst = new List<Product>();
+            string _statement = "SELECT ProductID,ProductName,Quantity from Products";
+            MySqlConnection _connection = GetConnection();
+
+            _connection.Open();
+
+            MySqlCommand _sqlcommand = new MySqlCommand(_statement, _connection);
+
+            using (MySqlDataReader _reader = _sqlcommand.ExecuteReader())
+            {
+                while (_reader.Read())
+                {
+                    Product _product = new Product()
+                    {
+                        ProductID = _reader.GetInt32(0),
+                        ProductName = _reader.GetString(1),
+                        Quantity = _reader.GetInt32(2)
+                    };
+
+                    _product_lst.Add(_product);
+                }
+            }
+            _connection.Close();
+            return _product_lst;
+        }
+
+    }
+}
+
--- src/tf-files/820050-mysql/commands.sh
+++ src/tf-files/820050-mysql/commands.sh
@@ -16,6 +16,18 @@ terraform plan -var-file="secrets.tfvars" -out main.tfplan
 
 terraform apply main.tfplan 
 
+# Once successifully provisioned, The next step is to load data into the database. run the data.sql script.
+
+# See the image AddData.jpg
+
+# Once the data is added, now prepare the project. Ensure the credentials are in place. 
+
+# Take a look at the following file.
+
+# https://github.com/AvtsVivek/Az204WthTerraform/blob/main/src/dotnet-apps/0040-ConnectToSqlapp/ConnectToSqlApp/Services/ProductService.cs
+
+dotnet run --project ./../../dotnet-apps/0150-ConnectToMySqlAppWithConString/ConnectToMySqlAppWithConString/ConnectToMySqlAppWithConString.csproj
+
 terraform state show 
 
 terraform show terraform.tfstate
