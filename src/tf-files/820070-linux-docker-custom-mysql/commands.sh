ce6a3166b892da23a883b8c3c9aaddf1813ff6dd
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2022-09-12 11:15:49 +0530

    New Project

---
+++ src/tf-files/820070-linux-docker-custom-mysql/commands.sh
@@ -0,0 +1,130 @@
+cd ../../..
+
+# cd into the directory.
+cd ./src/tf-files/820070-linux-docker-custom-mysql/
+
+cd ../../..
+
+cd ssh-keys
+
+# Run the following in bash prompt. In pwershell it will not work.
+ssh-keygen \
+    -m PEM \
+    -t rsa \
+    -b 4096 \
+    -C "azureuser@myserver" \
+    -f terraform-azure.pem 
+
+
+Get-ChildItem . 
+
+Get-ChildItem ./ssh-keys
+
+cd ssh-keys
+
+Rename-Item ./terraform-azure.pem.pub ./terraform-azure.pub
+
+cd ..
+
+terraform fmt
+
+terraform init
+
+terraform validate
+
+terraform plan -out main.tfplan
+
+# terraform show main.tfplan
+
+terraform apply main.tfplan 
+
+# Once successfully applied, Review the resources.
+# Download the topology diagram. Go to the created vnet and then diagram.
+# Also look at subnets and corresponding security groups.
+
+# Connect to the VM. Ensure the vm is running.
+# Note the IP address.
+# Run the following with the ip address.
+# Run the following in bash prompt.
+# If you get the following permission denined, 
+# azureuser@40.114.14.64: Permission denied (publickey,gssapi-keyex,gssapi-with-mic)
+# then you are not in the correct directory.
+
+cd ..
+
+# cd into the directory.
+cd ./src/tf-files/820060-linux-docker-mysql/
+
+ssh -i ssh-keys/terraform-azure.pem azureuser@20.169.167.33
+
+cd /var/log
+
+cat cloud-init-output.log
+
+sudo -i
+
+# Wait for at least 5 minutes. Then run the following commands.
+
+exit
+
+# Now that you are in the VM, you can run the following commands.
+
+hostname
+
+# Switch to the root user.
+sudo su -
+
+cd /var/log
+
+# Now look for cloud-init-output.log file.
+
+# use the tail command to see the last 100 lines.
+tail -100f cloud-init-output.log
+# Review what all happened as the vm was booting.
+
+# Now get the ip of the vm.
+
+# Browse to the ip address. You should see Nginx page. See the image NginxPage.jpg
+
+# You can also connect to my sql database. First install mysql workbench on your local computer.
+
+# Then you should be able to connect to it. See the image MySqlOnContainer.jpg
+
+# You can also get into the running my sql contianer.
+
+# First runt he following command to list the containers
+
+sudo docker contianer ls
+
+# Ensure the contianer named mysql-instance is presnet.
+
+# Now run the following to get into the container
+sudo docker exec -it mysql-instance mysql -uroot -p
+
+# When asked for the pass, give it(one you gave it during provisioning. sudo docker run --name mysql-instance -p 3306:3306 --restart on-failure -d -e MYSQL_ROOT_PASSWORD=H@Sh1CoR3! mysql)
+
+show databases;
+
+## Thats it.
+
+# Press Ctrl+C to comeout.
+
+terraform state list
+
+# To create diagram, run the following command.
+code . -r 
+# Ensure Azure Terraform extension is installed in VS Code
+# Press F1 and Run the command AzureTerraform: Visualize. Graph.png will be generated.
+
+# For the following command to work, you need to pass on the resource, or data source.
+# This resource or data source is got from terraform state list command
+
+terraform state show 
+
+terraform show terraform.tfstate
+
+terraform plan -destroy -out main.destroy.tfplan
+
+# terraform show main.destroy.tfplan
+
+terraform apply main.destroy.tfplan
---
+++ src/tf-files/820070-linux-docker-custom-mysql/tf7-05-web-linuxvm-resource.tf
@@ -0,0 +1,116 @@
+# Locals Block for custom data
+
+# sudo curl -L "https://dot.net/v1/dotnet-install.sh" -o /tmp/dotnet-install.sh
+# sudo chmod 777 /tmp/dotnet-install.sh
+# sudo /tmp/dotnet-install.sh -c 6.0
+# sudo /usr/sbin/setsebool httpd_can_network_connect 1
+# sudo /usr/sbin/setsebool -P httpd_can_network_connect 1
+
+locals {
+  webvm_custom_data = <<CUSTOM_DATA
+#!/bin/sh
+
+sudo systemctl stop firewalld
+sudo systemctl disable firewalld
+
+echo "Adding repo to config manager"
+
+sudo dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
+
+echo "Adding repo to config manager - DONE"
+
+echo "Starting to install docker-ce "
+
+sudo dnf install docker-ce -y
+
+echo "Starting to install docker-ce - DONE "
+
+echo "Starting to install docker-ce-cli "
+
+sudo dnf install docker-ce-cli -y
+
+echo "Starting to install docker-ce-cli - DONE "
+
+echo "Starting to install containerd.io"
+
+sudo dnf install containerd.io -y
+
+echo "Starting to install containerd.io - DONE "
+
+echo "Starting to install docker-compose-plugin"
+
+sudo dnf install docker-compose-plugin -y
+
+echo "Starting to install docker-compose-plugin - DONE "
+
+sudo systemctl start docker
+
+sudo systemctl start firewalld
+sudo systemctl enable firewalld
+
+sudo iptables -t filter -F
+iptables -t filter -X
+systemctl restart docker
+
+echo "Starting docker nginx conainer "
+
+sudo docker run --name appnginx -p 80:80 -d nginx
+
+echo "Starting docker nginx conainer - DONE"
+
+echo "Starting docker mysql conainer "
+
+sudo docker run --name mysql-instance -p 3306:3306 --restart on-failure -d -e MYSQL_ROOT_PASSWORD=H@Sh1CoR3! mysql
+
+echo "Starting docker mysql conainer - DONE"
+
+CUSTOM_DATA
+}
+
+
+# Resource: Azure Linux Virtual Machine
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/linux_virtual_machine
+resource "azurerm_linux_virtual_machine" "web_linuxvm" {
+  name = "${local.resource_name_prefix}-web-linuxvm"
+  # If the following computer_name is not set, it will be automatically be set to the name argument.
+  #computer_name = "web-linux-vm" # Hostname of the VM (Optional)
+
+  resource_group_name   = azurerm_resource_group.rg.name
+  location              = azurerm_resource_group.rg.location
+  size                  = "Standard_DS1_v2"
+  admin_username        = "azureuser"
+  network_interface_ids = [azurerm_network_interface.web_linuxvm_nic.id]
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = file("${path.module}/ssh-keys/terraform-azure.pub")
+  }
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+  source_image_reference {
+    publisher = "RedHat"
+    offer     = "RHEL"
+    sku       = "83-gen2"
+    version   = "latest"
+  }
+
+  # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/linux_virtual_machine#custom_data
+  # custom_data = filebase64("${path.module}/app-scripts/redhat-webvm-script.sh")
+  # custom_data = filebase64("${path.module}/app-scripts/app1-cloud-init.txt")
+  custom_data = base64encode(local.webvm_custom_data)
+
+  connection {
+    type        = "ssh"
+    host        = self.public_ip_address
+    user        = self.admin_username
+    password    = 123
+    private_key = file("${path.module}/ssh-keys/terraform-azure.pem")
+  }
+
+  # File Provisioner-1: Copies the file-copy.html file to /tmp/file-copy.html
+  provisioner "file" {
+    source      = "apps/file-copy.html"
+    destination = "/tmp/file-copy.html"
+  }
+}
