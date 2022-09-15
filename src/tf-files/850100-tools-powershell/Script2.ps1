f20c392998f43a209b1cc2dc753f2eb7227244f0
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2022-09-15 12:22:26 +0530

    Added more info

--- src/tf-files/850100-tools-powershell/Script2.ps1
+++ src/tf-files/850100-tools-powershell/Script2.ps1
@@ -15,9 +15,26 @@ https://docs.microsoft.com/en-us/powershell/module/az.resources/set-azresource?v
 $ResourceGroupName="powershell-grp"
 $WebAppName="compapp1122334455"
 
-
 $Properties =@{
-    repoUrl="https://github.com/alashro/webapp";
+    # repoUrl="https://github.com/alashro/webapp";
+    # If you use the following https://github.com/AvtsVivek/Az204WthTerraform/tree/main/src/dotnet-apps/0020-simple-webapp
+    # Then you get the following error.
+    # operation failed with Admin access is required for repository
+    # repoUrl="https://github.com/AvtsVivek/Az204WthTerraform/tree/main/src/dotnet-apps/0020-simple-webapp";
+
+    # If you use the following https://github.com/AvtsVivek/SimpleNet6WebApp
+    # Then you get the following error.
+    # Set-AzResource: C:\Vivek\Trials\Tf\Az204\src\tf-files\850100-tools-powershell\Script2.ps1:31:1
+    # Line |
+    #   31 |  Set-AzResource -ResourceGroupName $ResourceGroupName `
+    #      |  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    #      | The operation failed because resource is in the: 'Failed' state. Please check the logs for more details.
+
+    # I did not go deep into why this is happening. Leaving now.
+
+    # So use 
+    # repoUrl="https://github.com/alashro/webapp";
+    repoUrl="https://github.com/AvtsVivek/SimpleNet6WebApp";
     branch="master";
     isManualIntegration="true";
 }
