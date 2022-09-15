2bf6d334e2b4932b5515e20d305b9a915beaae71
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2022-09-15 13:59:16 +0530

    Added more info

---
+++ src/tf-files/850110-tools-cli/commands1.sh
@@ -0,0 +1,31 @@
+# Execute the following in powershell.
+
+$ResourceGroupName="docker-grp"
+
+$ResourceGroupLocation="North Europe"
+
+$AppServicePlanName="companyplan"
+
+$AppName="dockerapp665544"
+
+
+az group create --name $ResourceGroupName --location $ResourceGroupLocation
+
+az group list
+
+# 1. First we can create an App service plan
+
+az appservice plan create --name $AppServicePlanName --resource-group $ResourceGroupName --is-linux
+
+# 2. Then we create the web app
+
+az webapp create --resource-group $ResourceGroupName --plan $AppServicePlanName --name $AppName --deployment-container-image-name avts/nginxvivek:v4
+
+# 3. If you want to turn on container logging
+
+az webapp log config --name $AppName --resource-group $ResourceGroupName --docker-container-logging filesystem
+
+# 4. Enable the log stream
+
+az webapp log tail --name $AppName --resource-group $ResourceGroupName
+
