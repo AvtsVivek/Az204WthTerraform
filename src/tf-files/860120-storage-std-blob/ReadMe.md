1f17befd9d1275b52ff192a3e6c66f790b26fbf5
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2022-09-15 20:00:36 +0530

    New Project

---
+++ src/tf-files/860120-storage-std-blob/ReadMe.md
@@ -0,0 +1,5 @@
+# Storage Accounts
+
+- Here we go.
+
+- 
---
+++ src/tf-files/860120-storage-std-blob/tf5-resource-group.tf
@@ -0,0 +1,5 @@
+# Create Resource Group
+resource "azurerm_resource_group" "resource_group" {
+  name     = var.resource_group_name
+  location = var.location
+}
