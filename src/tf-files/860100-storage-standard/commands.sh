946d5438e5d59468090d5b8858a2028463bfba51
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2022-09-15 19:47:12 +0530

    Renamed folder

---
+++ src/tf-files/860100-storage-standard/commands.sh
@@ -0,0 +1,47 @@
+cd ../../..
+
+# cd into the directory.
+# C:\Vivek\Trials\Tf\Az204\src\tf-files\840100-aks-nginx\commands.sh
+
+cd ./src/tf-files/860100-storage-standard
+
+terraform fmt
+
+terraform init
+
+terraform validate
+
+terraform plan -out main.tfplan 
+
+# terraform show main.tfplan
+
+terraform apply main.tfplan
+
+terraform state list
+
+# For the following command to work, you need to pass on the resource, or data source.
+# This resource or data source is got from terraform state list command
+terraform state show 
+
+terraform show terraform.tfstate
+
+terraform plan -destroy -out main.destroy.tfplan
+
+# terraform show main.destroy.tfplan
+
+terraform apply main.destroy.tfplan
+
+Remove-Item -Recurse -Force .terraform/modules
+
+Remove-Item -Recurse -Force .terraform
+
+Remove-Item terraform.tfstate
+
+Remove-Item terraform.tfstate.backup
+
+Remove-Item main.tfplan
+
+Remove-Item main.destroy.tfplan
+
+Remove-Item .terraform.lock.hcl
+
--- src/tf-files/860100-storage/commands.sh
+++
@@ -1,9 +0,0 @@
-cd ../../..
-
-# cd into the directory.
-# C:\Vivek\Trials\Tf\Az204\src\tf-files\840100-aks-nginx\commands.sh
-
-cd ./src/tf-files/860100-strorage
-
-
-
