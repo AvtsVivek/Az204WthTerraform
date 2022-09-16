57a02678bc99d80cfa43db4fb20220503479d460
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2022-09-16 07:51:51 +0530

    New Project

---
+++ src/tf-files/860100-storage-std-static-web/tf6-storage-account.tf
@@ -0,0 +1,18 @@
+
+
+# Create Azure Storage account
+resource "azurerm_storage_account" "storage_account" {
+  name                = "${var.storage_account_name}${random_string.myrandom.id}"
+  resource_group_name = azurerm_resource_group.resource_group.name
+
+  location                 = var.location
+  account_tier             = var.storage_account_tier # Standard Or Premium
+  account_replication_type = var.storage_account_replication_type # valid options are LRS, GRS, RAGRS, ZRS, GZRS and RAGZRS
+  account_kind             = var.storage_account_kind
+
+  static_website {
+    index_document     = var.static_website_index_document
+    error_404_document = var.static_website_error_404_document
+  }
+}
+
