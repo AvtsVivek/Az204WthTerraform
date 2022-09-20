b876b5fa897cea7dee2101af412bf5367adfa9d0
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2022-09-20 16:49:40 +0530

    Added more info

--- src/tf-files/860525-table-storage-access-with-dotnet/ReadMe.md
+++ src/tf-files/860525-table-storage-access-with-dotnet/ReadMe.md
@@ -6,27 +6,21 @@ resource "azurerm_storage_table" "example" {
   storage_account_name = azurerm_storage_account.storage_account.name
 }
 
-resource "azurerm_storage_table_entity" "example" {
-
-  storage_account_name = azurerm_storage_account.storage_account.name
-  table_name           = azurerm_storage_table.example.name
-
-  partition_key = "Grocery"
-  row_key       = "Fruits"
-
-  entity = {
-    Price        = "100"
-    PriceUnit    = "Indian Rs"
-    Quantity     = "1"
-    QuantityUnit = "Kg"
-
-  }
-}
 ```
 
 - The config above creates the following resources.
 
 ![Table Storage](./images/1TableStorage1.jpg)
 
+- Once deployed, get the connection string.
 
 ![Table Storage](./images/1TableStorage2.jpg)
+
+
+
+![Table Storage](./images/1TableStorage3.jpg)
+
+![Table Storage](./images/1TableStorage4.jpg)
+
+![Table Storage](./images/1TableStorage5.jpg)
+
--- src/tf-files/860525-table-storage-access-with-dotnet/commands.sh
+++ src/tf-files/860525-table-storage-access-with-dotnet/commands.sh
@@ -16,8 +16,24 @@ terraform apply main.tfplan
 
 terraform state list
 
-# Once you download the file, you should see it in the folder. See the image 
+# Once deployed, get the connection string. See the image # 1TableStorage2.jpg
 
+
+# Update the Connection string.
+# https://github.com/AvtsVivek/Az204WthTerraform/blob/main/src/dotnet-apps/0170-TableStorage/TableStorage/Program.cs
+
+# var connectionString = "DefaultEndpointsProtocol=https;AccountName=staticwebsiteqmvtgi;AccountKey=PVqBjgwnvGRaVqqa93o0LYs4JGbP+vCfyGjBWVeNr58UnfsSKS9L7/jPXYOVc39jLUMVngTTmheb+AStAtXz6A==;EndpointSuffix=core.windows.net";
+# var tableName = "mysampletable";
+
+
+
+# Now the time to run the dotnet app. 
+dotnet build ./../../dotnet-apps/0170-TableStorage/TableStorage/TableStorage.csproj
+
+# For the following execute command to run successifully, the file 'data.sql' must be present in the following folder
+# ./src/tf-files/860325-storage-access-through-dotnet
+
+dotnet run --project ./../../dotnet-apps/0170-TableStorage/TableStorage/TableStorage.csproj
 
 # For the following command to work, you need to pass on the resource, or data source.
 # This resource or data source is got from terraform state list command
