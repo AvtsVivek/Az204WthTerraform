2a3bfc1a5fbb283418d7cfd2870b49fdb22dd8d4
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2022-09-22 16:51:22 +0530

    Added more info

--- src/tf-files/870150-cosmos/tf6-cosmos-db.tf
+++ src/tf-files/870150-cosmos/tf6-cosmos-db.tf
@@ -53,20 +53,3 @@ resource "azurerm_cosmosdb_sql_database" "cosmos_db" {
   account_name        = azurerm_cosmosdb_account.cosmos_db_account.name
   throughput          = 400
 }
-
-resource "azurerm_cosmosdb_sql_container" "category_container" {
-  name                  = "vivek-cosmos-sql-db-category-container"
-  resource_group_name   = azurerm_cosmosdb_account.cosmos_db_account.resource_group_name
-  account_name          = azurerm_cosmosdb_account.cosmos_db_account.name
-  database_name         = azurerm_cosmosdb_sql_database.cosmos_db.name
-  partition_key_path    = "/category"
-  partition_key_version = 1
-}
-
-resource "azurerm_cosmosdb_sql_container" "grocery_source_container" {
-  name                = "vivek-cosmos-sql-db-grocery-source-container"
-  resource_group_name = azurerm_cosmosdb_account.cosmos_db_account.resource_group_name
-  account_name        = azurerm_cosmosdb_account.cosmos_db_account.name
-  database_name       = azurerm_cosmosdb_sql_database.cosmos_db.name
-  partition_key_path  = "/grocerysource"
-}
