dda2c5b9a0cdf762215585b8acc943cd550b8a94
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2022-09-23 12:02:02 +0530

    Added more info

---
+++ src/tf-files/870225-cosmos-table-api-dotnet-access/commands.sh
@@ -0,0 +1,60 @@
+cd ../../..
+
+cd ./src/tf-files/870200-cosmos-table-api
+
+terraform fmt
+
+terraform init
+
+terraform validate
+
+terraform plan -out main.tfplan 
+
+# terraform show main.tfplan
+
+terraform apply main.tfplan
+
+terraform state list
+
+# Once deployed, get the connection string. See the image # 1TableStorage2.jpg
+
+# Update the Connection string.
+# https://github.com/AvtsVivek/Az204WthTerraform/blob/main/src/dotnet-apps/0170-TableStorage/TableStorage/Program.cs
+
+# var connectionString = "DefaultEndpointsProtocol=https;AccountName=staticwebsiteqmvtgi;AccountKey=PVqBjgwnvGRaVqqa93o0LYs4JGbP+vCfyGjBWVeNr58UnfsSKS9L7/jPXYOVc39jLUMVngTTmheb+AStAtXz6A==;EndpointSuffix=core.windows.net";
+# var tableName = "mysampletable";
+
+
+
+# Now the time to run the dotnet app. 
+dotnet build ./../../dotnet-apps/0170-TableStorage/TableStorage/TableStorage.csproj
+
+# For the following execute command to run successifully, the file 'data.sql' must be present in the following folder
+# ./src/tf-files/860325-storage-access-through-dotnet
+
+dotnet run --project ./../../dotnet-apps/0170-TableStorage/TableStorage/TableStorage.csproj
+
+terraform state show 
+
+terraform show terraform.tfstate
+
+terraform plan -destroy -out main.destroy.tfplan
+
+# terraform show main.destroy.tfplan
+
+terraform apply main.destroy.tfplan
+
+Remove-Item -Recurse -Force .terraform/modules
+
+Remove-Item -Recurse -Force .terraform
+
+Remove-Item terraform.tfstate
+
+Remove-Item terraform.tfstate.backup
+
+Remove-Item main.tfplan
+
+Remove-Item main.destroy.tfplan
+
+Remove-Item .terraform.lock.hcl
+
