a3e24e2be46d1c127825db19533e8c0dd6d77db3
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2022-09-23 18:58:09 +0530

    Added files

--- src/tf-files/700100-azure-func/commands.sh
+++ src/tf-files/700100-azure-func/commands.sh
@@ -17,20 +17,6 @@ terraform apply main.tfplan
 
 terraform state list
 
-# Once successifully provisioned, the next step is to get the access key.
-
-# See the image AppConfiguration1.jpg in the images folder.
-
-# Take a look at the following file.
-
-# https://github.com/AvtsVivek/Az204WthTerraform/blob/9c325df5ee389ce3e2b742a39f3f1bf62fa43834/src/dotnet-apps/0060-ConnectToAzureConfig/ConnectToAzureConfig/Program.cs#L6
-
-# Get the access key and update it.
-
-dotnet run --project ./../../dotnet-apps/0060-ConnectToAzureConfig/ConnectToAzureConfig/ConnectToAzureConfig.csproj
-
-# See the image AppConfiguration4.jpg in the images folder.
-
 terraform state show 
 
 terraform show terraform.tfstate
