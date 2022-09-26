d3038a9f60c8a5bc003a5973bd90c9b43b90818f
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2022-09-26 13:21:27 +0530

    New Project

---
+++ src/tf-files/400200-security-user-creation/tf2-active-directory-user.tf
@@ -0,0 +1,6 @@
+resource "azuread_user" "ad_user" {
+  user_principal_name = "aduser2@vivek7dm1outlook.onmicrosoft.com"
+  display_name        = "aduser2"
+  mail_nickname       = "aduser2"
+  password            = "vivek@123" # Passwords should not be handled like this. User secrets.tfvars file.
+}
