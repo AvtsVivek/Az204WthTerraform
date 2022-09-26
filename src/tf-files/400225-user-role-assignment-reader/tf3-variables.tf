68efdc58c0e0cc5951ecdcd3f7ff459f7487d785
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2022-09-26 15:34:09 +0530

    New Project

---
+++ src/tf-files/400225-user-role-assignment-reader/tf3-variables.tf
@@ -0,0 +1,44 @@
+# Input variable definitions
+
+variable "location" {
+  description = "The Azure Region in which all resources groups should be created."
+  type        = string
+}
+
+
+variable "resource_group_name_suffix" {
+  description = "The suffix for the resource group"
+  type        = string
+}
+
+variable "resource_group_name_prefix" {
+  description = "The prefix for the resource group"
+  type        = string
+}
+
+# Input variable definitions
+
+variable "storage_account_name" {
+  description = "The name of the storage account"
+  type        = string
+}
+
+variable "storage_account_tier" {
+  description = "Storage Account Tier"
+  type        = string
+}
+
+variable "storage_account_replication_type" {
+  description = "Storage Account Replication Type"
+  type        = string
+}
+
+variable "storage_account_kind" {
+  description = "Storage Account Kind"
+  type        = string
+}
+
+variable "static_files_list" {
+  default     = ["blob1.txt"]
+  description = "Static file list."
+}
