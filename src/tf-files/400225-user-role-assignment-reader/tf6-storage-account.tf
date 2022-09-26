54697a812e4495c1fd77add4de96380bcbe21253
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2022-09-26 15:46:44 +0530

    New Project

--- src/tf-files/400225-user-role-assignment-reader/tf6-storage-account.tf
+++ src/tf-files/400225-user-role-assignment-reader/tf6-storage-account.tf
@@ -11,27 +11,3 @@ resource "azurerm_storage_account" "storage_account" {
   account_kind             = var.storage_account_kind
 
 }
-
-# resource "azurerm_storage_container" "asc" {
-#   name                  = "content"
-#   storage_account_name  = azurerm_storage_account.storage_account.name
-#   container_access_type = "container" # Possible values are blob, container or private. Defaults to private.
-# }
-
-# resource "azurerm_storage_blob" "static_files_html" {
-
-#   for_each               = toset(var.static_files_list)
-#   name                   = each.value
-#   storage_account_name   = azurerm_storage_account.storage_account.name
-#   storage_container_name = azurerm_storage_container.asc.name
-#   type                   = "Block"
-#   content_type           = "text/html"
-#   source                 = each.value
-
-#   access_tier = "Archive"
-
-#   depends_on = [
-#     azurerm_resource_group.resource_group,
-#     azurerm_storage_account.storage_account
-#   ]
-# }
