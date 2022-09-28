b6f88ef2bfe53e7d4c567785450262c9fe9ad926
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2022-09-28 17:52:35 +0530

    Added more info

--- src/tf-files/400300-add-owner-to-subscription/tf2-role-assignment.tf
+++ src/tf-files/400300-add-owner-to-subscription/tf2-role-assignment.tf
@@ -8,10 +8,7 @@ data "azurerm_subscription" "current_sub" {}
 resource "azurerm_role_assignment" "reader_role" {
   scope                = data.azurerm_subscription.current_sub.id
   role_definition_name = "Owner"
-  # role_definition_name = "Storage Account Contributor"
-  # principal_id         = azurerm_user_assigned_identity.appag_umid.principal_id
   principal_id = data.azuread_client_config.current.object_id
-  # principal_id         = azurerm_kubernetes_cluster.kubweb.kubelet_identity[0].object_id
 }
 
 
