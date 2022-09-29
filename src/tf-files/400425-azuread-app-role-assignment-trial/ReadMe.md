a2592342e1ba39ced975dbbae6aabb1a50e556e2
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2022-09-29 19:44:44 +0530

    Renamed files

---
+++ src/tf-files/400425-azuread-app-role-assignment-trial/ReadMe.md
@@ -0,0 +1,2 @@
+# Trying the [example in this one](https://registry.terraform.io/providers/hashicorp/azuread/latest/docs/resources/app_role_assignment).
+
---
+++ src/tf-files/400425-azuread-app-role-assignment-trial/tf3-service-principal.tf
@@ -0,0 +1,15 @@
+# resource "azuread_service_principal" "app_sp" {
+#   application_id = azuread_application.webapp.application_id
+
+#   app_role_assignment_required = false
+#   owners                       = [data.azuread_client_config.current.object_id]
+# }
+
+resource "azuread_service_principal" "msgraph" {
+  application_id = data.azuread_application_published_app_ids.well_known.result.MicrosoftGraph
+  use_existing   = true
+}
+
+resource "azuread_service_principal" "example" {
+  application_id = azuread_application.example.application_id
+}
