ecc108437f16f752fa1b1f2780cc46d9e5790a84
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2022-09-30 13:26:42 +0530

    Edit and formatting

--- src/tf-files/400400-azuread-app-graph-permissions/tf8-role-assignment.tf
+++ src/tf-files/400400-azuread-app-graph-permissions/tf8-role-assignment.tf
@@ -9,7 +9,7 @@
 # }
 
 
-resource "azuread_app_role_assignment" "example" {
+resource "azuread_app_role_assignment" "app_role" {
   app_role_id         = azuread_service_principal.msgraph.app_role_ids["User.Read.All"]
   principal_object_id = azuread_service_principal.sp_app.object_id
   resource_object_id  = azuread_service_principal.msgraph.object_id
