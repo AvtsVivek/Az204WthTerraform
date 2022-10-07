1cf7f66959fa746b7028b5dcb12a40d7895c75d4
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2022-10-07 20:25:09 +0530

    Added more info

--- src/tf-files/890305-win-app-to-key-vault-user-assigned/tf10-2-win-web-app.tf
+++ src/tf-files/890305-win-app-to-key-vault-user-assigned/tf10-2-win-web-app.tf
@@ -30,9 +30,17 @@ resource "azurerm_windows_web_app" "windows_web_app" {
     # type = "SystemAssigned"
   }
 
+  # In order to use a user assigned identity with the DefaultAzureCredential you must
+  # specify the client id of the user assigned identity.
+  # You can do this either by passing it in code via
+  # DefaultAzureCredentialOptions.ManagedIdentityClientId like so:
+  # var tokenCredential = new DefaultAzureCredential(new DefaultAzureCredentialOptions { ManagedIdentityClientId = "bc783ad4-77a7-4d5d-9151-8e9aad5d9fec" });
+  # Alternatively you can also specify the client id to be used via the AZURE_CLIENT_ID environment variable.
+  # https://github.com/Azure/azure-sdk-for-net/issues/13564#issuecomment-660431926
+  # https://github.com/Azure/azure-sdk-for-net/issues/11400#issuecomment-620179175
+  # For our case, we will be setting env var AZURE_CLIENT_ID in the terraform config file.
+
   app_settings = {
-    # https://github.com/Azure/azure-sdk-for-net/issues/13564#issuecomment-660431926
-    # https://github.com/Azure/azure-sdk-for-net/issues/11400#issuecomment-620179175
     "AZURE_CLIENT_ID" = azurerm_user_assigned_identity.appag_umid.client_id
   }
 
