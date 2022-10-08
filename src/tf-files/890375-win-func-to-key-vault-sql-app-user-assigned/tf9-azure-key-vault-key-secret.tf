57ec00feffea5af1f085ae99e44f112665b14794
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2022-10-08 11:45:44 +0530

    New Project

---
+++ src/tf-files/890375-win-func-to-key-vault-sql-app-user-assigned/tf9-azure-key-vault-key-secret.tf
@@ -0,0 +1,7 @@
+
+resource "azurerm_key_vault_secret" "key_vault_secret" {
+  name         = "sql-connection-string"
+  value        = "Server=tcp:hr-dev-ms-sql-server.database.windows.net,1433;Initial Catalog=hr-dev-ms-sql-db;Persist Security Info=False;User ID=adm1n157r470r;Password=4-v3ry-53cr37-p455w0rd;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+  key_vault_id = azurerm_key_vault.key_vault.id
+}
+
