e64fb046ebf1aa121791f8a06680b5efe1e522f6
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2022-10-09 21:58:44 +0530

    Added more info

--- src/tf-files/401550-add-owner-to-subscription/tf2-role-assignment.tf
+++ src/tf-files/401550-add-owner-to-subscription/tf2-role-assignment.tf
@@ -6,7 +6,7 @@ data "azurerm_subscription" "current_sub" {}
 
 
 resource "azurerm_role_assignment" "reader_role" {
-  scope                = data.azurerm_subscription.current_sub.id
+  scope = data.azurerm_subscription.current_sub.id
   # https://learn.microsoft.com/en-us/azure/role-based-access-control/built-in-roles
   role_definition_name = "Owner"
   principal_id         = data.azuread_client_config.current.object_id
