6b6c4f8eac7b15b0cb813993c3e3bed4ddfe768b
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2022-10-10 09:29:44 +0530

    Added more info

--- src/tf-files/401210-azuread-app-graph-update-user/tf3-service-principal.tf
+++ src/tf-files/401210-azuread-app-graph-update-user/tf3-service-principal.tf
@@ -16,7 +16,8 @@ resource "azuread_service_principal" "sp_app" {
   # What is the significance of the following feature_tags block? If we dont have this, then 
   # You will NOT see enterprise application in AAD -> Enterprise Applications -> All Application
   # The application is there, but it will not be visible, its filtered out.
-
+  # See the notes in the follwing file
+  # https://github.com/AvtsVivek/Az204WthTerraform/blob/main/src/tf-files/401100-app-registration-service-principal/tf6-service-principal.tf
   feature_tags {
     enterprise = true
     hide       = true
